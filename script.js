47faf36c72ee012d9092f84f9e4e1db4dc5d47e3
justice0997 <ararilejustice@gmail.com>
2026-06-26 14:07:58 -0400

    Add scroll-to-top and button click functionalities

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("topBtn").onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}; document.querySelector("button").onclick = function () {
+  alert("Thank you for visiting Justice's Website!");
+};document.querySelector("button").onclick = function () {
+  document.querySelector("button").textContent = "welcome to my page i really appreciate!";
+};
